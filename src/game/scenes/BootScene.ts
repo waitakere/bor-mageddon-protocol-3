27794a694111d14f8fc7a46fac739b7d62402f25
ivanatag <245275551+ivanatag@users.noreply.github.com>
2026-03-17 00:30:09 +0000

    boot

--- src/game/scenes/BootScene.ts
+++ src/game/scenes/BootScene.ts
@@ -4,26 +4,22 @@ export class BootScene extends Phaser.Scene {
     constructor() { super({ key: 'BootScene' }); }
 
     preload() {
-        // Environments & Effects
         this.load.image('part1_sky', 'assets/images/environments/part1_sky.png');
         this.load.image('part1_mid', 'assets/images/environments/part1_mid.png');
         this.load.image('part1_floor', 'assets/images/environments/part1_floor.png');
         this.load.image('explosion_01', 'assets/images/environments/explosion_01.png');
 
-        // Items (Health Pickups)
         this.load.image('item-burek', 'assets/images/environments/item-burek.png');
         this.load.image('item-coffee', 'assets/images/environments/item-coffee.png');
         this.load.image('item-pork', 'assets/images/environments/item-pork.png');
         this.load.image('item-beer', 'assets/images/environments/item-beer.png');
         this.load.image('item-sandwich', 'assets/images/environments/item-sandwich.png');
 
-        // Atlases
         this.load.atlas('marko', 'assets/sprites/marko.png', 'assets/sprites/marko.json');
         this.load.atlas('maja', 'assets/sprites/maja.png', 'assets/sprites/maja.json');
         this.load.atlas('darko', 'assets/sprites/darko.png', 'assets/sprites/darko.json');
         this.load.atlas('enemies_1993', 'assets/sprites/enemies_1993.png', 'assets/sprites/enemies_1993.json');
 
-        // Audio
         this.load.audioSprite('sfx_atlas', 'assets/audio/sfx_atlas.json', ['assets/audio/sfx_atlas.mp3']);
     }
 
@@ -41,6 +37,7 @@ export class BootScene extends Phaser.Scene {
         const actionMap = { 
             'idle': 'idle', 'walk': 'walk', 'run': 'run', 
             'punch-1': 'punch-1', 'punch-2': 'punch-2', 'kick-1': 'kick-1', 'kick-2': 'kick-2', 
+            'jump-punch': 'jump-punch', 'jump-kick': 'jump-kick', // ADDED AERIAL COMBAT
             'damage': 'damage', 'die': 'knockdown', 'special': 'special', 'finisher': 'finisher' 
         };
 
@@ -48,7 +45,7 @@ export class BootScene extends Phaser.Scene {
             Object.entries(actionMap).forEach(([actionKey, folderName]) => {
                 const animKey = `${char}-${actionKey}`; 
                 const framePrefix = `${char}-${folderName}/frame_`;
-                const fps = ['punch-1', 'punch-2', 'kick-1', 'kick-2', 'special', 'finisher'].includes(actionKey) ? 15 : 10;
+                const fps = ['punch-1', 'punch-2', 'kick-1', 'kick-2', 'jump-punch', 'jump-kick', 'special', 'finisher'].includes(actionKey) ? 15 : 10;
                 const isLoop = ['idle', 'walk', 'run'].includes(actionKey);
                 this.createAutoAnimation(char, animKey, framePrefix, isLoop, fps);
             });
